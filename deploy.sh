6b798fe56273bac2b41b90cdf461984e30f71c7e
Bryant Huang <glossa@gmail.com>
2017-09-04 22:53:07 -0700

    Rebuild site on Mon, Sep  4, 2017 10:53:07 PM

--- deploy.sh
+++ deploy.sh
@@ -18,3 +18,5 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 git subtree push --prefix=public git@github.com:bbhh/biblefoundry-website.git gh-pages
+
+cp CNAME docs/
